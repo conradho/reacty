fe73a35874cf55e25b313a04084722e4d1c600af
conrad <conrad@pythonanywhere.com>
2015-11-13 15:57:42 +0000

    made js file jsx and compatible with eslint

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+/*eslint-env commonjs */
+
+var React = require('react');   //eslint-disable-line no-unused-vars
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
+
